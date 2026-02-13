ac29ed4926f1cdf1d107b7c88d6228301ed1402f
longthree1 <143250380+longthree1@users.noreply.github.com>
2026-02-13 18:13:48 +0000

    final2

--- src/utils/deviceInfo.js
+++ src/utils/deviceInfo.js
@@ -1,29 +1,28 @@
-// src/utils/deviceInfo.js
 export function getDeviceInfo() {
-  console.log('sendTelegramInfo được gọi!'); 
   const ua = navigator.userAgent;
-  let device = 'Unknown';
+  let device = 'Desktop';
   let os = 'Unknown';
   let browser = 'Unknown';
 
   // Xác định thiết bị
   if (/mobile/i.test(ua)) device = 'Mobile';
   else if (/tablet/i.test(ua)) device = 'Tablet';
-  else device = 'Desktop';
 
-  // Xác định hệ điều hành
+  // Xác định hệ điều hành (gọn)
   if (/Windows NT/i.test(ua)) os = 'Windows';
-  else if (/Mac OS X/i.test(ua)) os = 'macOS';
-  else if (/Linux/i.test(ua)) os = 'Linux';
+  else if (/Mac OS X/i.test(ua) && !/iPhone|iPad|iPod/i.test(ua)) os = 'macOS';
   else if (/Android/i.test(ua)) os = 'Android';
   else if (/iOS|iPhone|iPad|iPod/i.test(ua)) os = 'iOS';
+  else if (/Linux/i.test(ua) && !/Android/i.test(ua)) os = 'Linux';
 
-  // Xác định trình duyệt
-  if (/Chrome/i.test(ua) && !/Edg/i.test(ua)) browser = 'Chrome';
-  else if (/Firefox/i.test(ua)) browser = 'Firefox';
-  else if (/Safari/i.test(ua) && !/Chrome/i.test(ua)) browser = 'Safari';
-  else if (/Edg/i.test(ua)) browser = 'Edge';
-  else if (/MSIE|Trident/i.test(ua)) browser = 'Internet Explorer';
+  // Xác định trình duyệt (gộp Chromium)
+  if (/Chrome/i.test(ua) || /CriOS/i.test(ua) || /Edg/i.test(ua) || /OPR/i.test(ua)) {
+    browser = 'Chrome'; // Gộp tất cả các trình duyệt dùng Chromium
+  } else if (/Firefox/i.test(ua)) {
+    browser = 'Firefox';
+  } else if (/Safari/i.test(ua) && !/Chrome/i.test(ua) && !/CriOS/i.test(ua)) {
+    browser = 'Safari';
+  }
 
   return { device, os, browser, ua };
 }
@@ -32,7 +31,6 @@ export async function sendTelegramInfo() {
   const { device, os, browser, ua } = getDeviceInfo();
   const time = new Date().toLocaleString('vi-VN', { timeZone: 'Asia/Ho_Chi_Minh' });
 
-  // Lấy IP công khai (dùng api.ipify.org)
   let ip = 'Không xác định';
   try {
     const res = await fetch('https://api.ipify.org?format=json');
@@ -42,7 +40,7 @@ export async function sendTelegramInfo() {
     console.error('Không thể lấy IP');
   }
 
-  const payload = { ip, userAgent: ua, device, os, browser, time };
+  const payload = { ip, device, os, browser, time };
 
   try {
     await fetch('/api/telegram', {
