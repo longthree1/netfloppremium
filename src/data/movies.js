dc07d134e41ca3bd23fdd69f29b61254a99c4d28
longthree1 <143250380+longthree1@users.noreply.github.com>
2026-02-14 07:51:44 +0000

    fix ui vip

--- src/data/movies.js
+++ src/data/movies.js
@@ -3,7 +3,7 @@ export const bannerMovies = [
     id: 999,
     title: 'HUYỀN THOẠI MIXI',
     poster: '/images/mixi.png',
-    youtubeId: 'PD61lIYrG-M', // Thay ID YouTube bạn muốn
+    youtubeId: 'PD61lIYrG-M', // 
     description: 'From Mixi with love',
   }
 ];
@@ -21,15 +21,13 @@ export const nowPlaying = [
     id: 2,
     title: 'THÁNH GIÓNG 2025',
     poster: '/images/2025.png',
-    banner: 'https://img.phimmoichill.now/images/info/project-y.jpg',
     description: '-MADE BY NHÓM ĐẸP TRAI- \nThánh Gióng \nTừ một cậu bé làng Gióng lên ba không biết nói, không biết cười, vươn mình hóa thành tráng sĩ cao lớn, cưỡi ngựa sắt, nhổ tre làng đánh đuổi giặc Ân xâm lược. Hành trình kỳ vĩ của người anh hùng làng Gióng, biểu tượng bất diệt cho tinh thần yêu nước và sức mạnh chống ngoại xâm của dân tộc Việt Nam.',
     youtubeId: '6xMWK8Sy7eU',
   },
   {
     id: 3,
     title: 'Alo, Vũ à Vũ',
-    poster: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5xG6ifHifoo24bDEPHmlNGxplDMTjShrQfQ&s',
-    banner: '/images/loi-nho-banner.jpg',
+    poster: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5xG6ifHifoo24bDEPHmlNGxplDMTjShrQfQ&s',   
     description: '- Mình là Độ - \n- Thỉnh thoảng rảnh chó ko có gì làm mình hay edit clip vui, tất cả đều do team mình chơi tự nhiên, trong clip có khá nhiều từ ngữ bậy bạ nên các bạn lưu ý xung quanh trước khi xem - ',
     youtubeId: 'oyEOohYtgvI',
   },
